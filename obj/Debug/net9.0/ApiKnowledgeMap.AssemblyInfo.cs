38d6871135ed7604f02d281f7e4697c1ad00cb16
laio888 <santiagorueda1125985@gmail.com>
2026-03-08 10:43:04 -0500

    agregacion db

--- obj/Debug/net9.0/ApiKnowledgeMap.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiKnowledgeMap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiKnowledgeMap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2e964722e6a56301c86ad141187e90ff1e6dcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7160d32435b6d78c8e89676adbfc2b89db26d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiKnowledgeMap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiKnowledgeMap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
